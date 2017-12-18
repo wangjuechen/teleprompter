94c2e2b94a56d309d7b74c5e72214fc9de847918
Juechen <wjc.zhw@gmail.com>
2017-12-18 20:18:52 +1300

    Day3
    RecyclerView finished. Making MVVM construct for this app.

--- app/src/main/java/com/example/android/teleprompter/Adaptor/RecyclerViewAdaptors.java
+++ app/src/main/java/com/example/android/teleprompter/Adaptor/RecyclerViewAdaptors.java
@@ -5,34 +5,49 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.android.teleprompter.R;
 
-public class RecyclerViewAdaptors extends RecyclerView.Adapter{
+public class RecyclerViewAdaptors extends RecyclerView.Adapter<RecyclerViewAdaptors.documentsListViewHolder>{
 
+    public RecyclerViewAdaptors(){
+
+    }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public documentsListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).
                 inflate(R.layout.recyclerview_list_content, parent, false);
         return new documentsListViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(documentsListViewHolder holder, int position) {
+        holder.file_title.setText("Title");
+        holder.file_info.setText("Opened at 4th Oct");
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 1;
     }
 
     public class documentsListViewHolder extends RecyclerView.ViewHolder {
 
+        ImageView file_icon;
+        TextView file_title;
+        TextView file_info;
 
         public documentsListViewHolder(View itemView) {
+
             super(itemView);
+
+            View contentView = itemView;
+            file_icon = contentView.findViewById(R.id.iv_fileTypeIcon);
+            file_title = contentView.findViewById(R.id.tv_fileTitle);
+            file_info = contentView.findViewById(R.id.tv_fileInfo);
         }
     }
 
--- app/src/main/java/com/example/android/teleprompter/MainActivityFragment.java
+++ app/src/main/java/com/example/android/teleprompter/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.teleprompter;
 
+import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.android.teleprompter.Adaptor.RecyclerViewAdaptors;
 
@@ -21,6 +23,8 @@ public class MainActivityFragment extends Fragment {
 
     private RecyclerView mRecyclerView_List;
 
+    private TextView mTextView_listSubtitle;
+
     private final String BUNDLE_RECYCLE_LAYOUT = "recycler_view_bundle";
 
     private RecyclerViewAdaptors mAdaptor;
@@ -31,20 +35,28 @@ public class MainActivityFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mAdaptor = new RecyclerViewAdaptors();
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
-        //ButterKnife.bind(this, root);
         mRecyclerView_List = root.findViewById(R.id.rv_list);
+
+        mTextView_listSubtitle = root.findViewById(R.id.tv_list_subtitle);
 
         mLinearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
 
         mRecyclerView_List.setLayoutManager(mLinearLayoutManager);
 
-        mRecyclerView_List.setHasFixedSize(true);
-
         mRecyclerView_List.setAdapter(mAdaptor);
+
+        mTextView_listSubtitle.setText("Today");
 
         return root;
     }
@@ -53,7 +65,8 @@ public class MainActivityFragment extends Fragment {
     public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
         super.onViewStateRestored(savedInstanceState);
         if(savedInstanceState != null){
-
+            Parcelable savedRecyclerLayoutState = savedInstanceState.getParcelable(BUNDLE_RECYCLE_LAYOUT);
+            mRecyclerView_List.getLayoutManager().onRestoreInstanceState(savedRecyclerLayoutState);
         }
 
     }
